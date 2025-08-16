9c22be8e98c5612f9659325d083fb001606d1cbd
Julien Nury <julien@nury.ch>
2025-08-16 11:21:03 +0200

    fix singlepage routing

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  preview: {
+    port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
